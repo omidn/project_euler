77919a4d876189d113c539df9c96c7128104ca8d
Omid Najaee <o.najaee@gmail.com>
2018-09-18 18:27:10 +0200

    add comments

--- problem-2.py
+++ problem-2.py
@@ -1,4 +1,5 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler002
+
 def fibo(n):
     if (n == 2):
         return 2
@@ -6,6 +7,7 @@ def fibo(n):
         return 8
     return 4 * fibo(n - 3) + fibo(n - 6)
 
+# cache all values
 dict = {}
 MAX = 4*10**16
 
@@ -18,6 +20,7 @@ t = int(input().strip())
 
 for i in range(t):
     upper_limit = int(input().strip())
+    # sum only terms which do not exceed the upper_limit
     sum = 0
     counter = 2
     while (dict[counter] < upper_limit):
